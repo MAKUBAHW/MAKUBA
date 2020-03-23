8167bd3bfdaf272bf4fb724f50ae13502aff066f
MAKUBAHW <ictschoool27@gmail.comls>
2020-03-23 06:13:05 +0300

    forex 1 project

--- DNT.py
+++ DNT.py
@@ -1,14 +1,14 @@
-# a,b,c,d=30,.38,'makuba kenya',false
-# print('a=' ,a, 'type=' ,type (a))
-# print(b, type(b))
-# print("c=" ,b, "type=", type (b))
-# print(d, type(d))
-# age=int(input("enter your age:"))
-# print("your age is",age, "type=" ,type(age))
-# a=age+1.7
-# print(a)
-# a=30
-# A=40
+ a,b,c,d=30,.38,'makuba kenya',false
+print('a=' ,a, 'type=' ,type (a))
+ print(b, type(b))
+ print("c=" ,b, "type=", type (b))
+ print(d, type(d))
+ age=int(input("enter your age:"))
+ print("your age is",age, "type=" ,type(age))
+ a=age+1.7
+ print(a)
+ a=50
+ A=40
 print(a, type(a), id(a))
 print(A, type(A), id(A))
 a,b=20,3
